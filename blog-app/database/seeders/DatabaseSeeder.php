eff4f465115a8e067170236a704ea25139cc5c45
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-05-03 02:55:32 +0200

    Migration for rankings

--- blog-app/database/seeders/DatabaseSeeder.php
+++ blog-app/database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Ranking;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,5 +14,17 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(1)->create();
         \App\Models\Post::factory(1)->create();
         \App\Models\Comment::factory(1)->create();
+
+        $rankings = [
+            ['name' => 'Rang 1', 'min_score' => 20],
+            ['name' => 'Rang 2', 'min_score' => 40],
+            ['name' => 'Rang 3', 'min_score' => 60],
+            ['name' => 'Rang 4', 'min_score' => 80],
+            ['name' => 'Rang 5', 'min_score' => 100],
+        ];
+
+        foreach ($rankings as $ranking) {
+            Ranking::create($ranking);
+        }
     }
 }
