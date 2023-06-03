b7a8d7712225d1332dff244ae74f1542532e9e43
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-06-03 23:29:05 +0200

    Add 404 page

---
+++ blog-app/resources/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<div class="flex items-center justify-center min-h-screen">
+    <div class="max-w-md mx-auto">
+        <div class="text-center">
+            <h1 class="text-6xl font-bold text-gray-800">404</h1>
+            <p class="mt-2 text-2xl font-semibold text-gray-800">Strona nie została odnaleziona</p>
+            <p class="mt-4 text-gray-600">Strona której szukasz prawdopodobnie nie istnieje.</p>
+        </div>
+        <div class="mt-8 flex justify-center">
+            <a href="/" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded">
+                Powrót do strony głównej
+            </a>
+        </div>
+    </div>
+</div>
