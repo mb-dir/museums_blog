0b25a4ca0cbed0885e9fd2f749beed665b9de8ca
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-06-03 22:36:00 +0200

    Seeder refactor

--- blog-app/database/seeders/DatabaseSeeder.php
+++ blog-app/database/seeders/DatabaseSeeder.php
@@ -5,14 +5,15 @@ use App\Models\User;
 use App\Models\Comment;
 use App\Models\Ranking;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder {
+class DatabaseSeeder extends Seeder
+{
     /**
      * Seed the application's database.
      */
-    public function run(): void {
+    public function run(): void
+    {
         $rankings = [
             ['name' => 'Nowicjusz Muzealny', 'min_score' => 20],
             ['name' => 'Odkrywca Kultury', 'min_score' => 40],
@@ -24,39 +25,48 @@ class DatabaseSeeder extends Seeder {
         foreach ($rankings as $ranking) {
             Ranking::create($ranking);
         }
-        User::factory(6)
-            ->create()
-            ->each(function ($user, $index) {
-                if ($index === 1 || $index === 2 || $index === 3) {
-                    Post::factory(4)->create(['user_id' => $user->id]);
+
+        $users = User::factory(10)->create();
+
+        $userData = [
+            1 => ['posts' => 1],
+            2 => ['posts' => 4],
+            3 => ['posts' => 6, 'comments' => 4],
+            4 => ['comments' => 1],
+        ];
+
+        foreach ($users as $index => $user) {
+            if (isset($userData[$index])) {
+                if (isset($userData[$index]['posts'])) {
+                    Post::factory($userData[$index]['posts'])->create(['user_id' => $user->id]);
                 }
 
-                if ($index === 3 || $index === 4 || $index === 8) {
-                    Comment::factory(4)->create(['user_id' => $user->id]);
+                if (isset($userData[$index]['comments'])) {
+                    Comment::factory($userData[$index]['comments'])->create(['user_id' => $user->id]);
                 }
+            }
 
-                $userScore = $user->posts()->sum('score') + $user->comments()->sum('score');
-                $user->score = $userScore;
+            $userScore = $user->posts()->sum('score') + $user->comments()->sum('score');
+            $user->score = $userScore;
 
-                $ranking_indexes = [1, 2, 3, 4, 5];
-                foreach($ranking_indexes as $rank){
-                    if($user->score >= $rank * 20){
-                        $user->rankings()->syncWithoutDetaching([$rank]);
-                    }
+            $rankingIndexes = [1, 2, 3, 4, 5];
+            foreach ($rankingIndexes as $rank) {
+                if ($user->score >= $rank * 20) {
+                    $user->rankings()->syncWithoutDetaching([$rank]);
                 }
+            }
 
-                $user->save();
-        });
-        // create admin
-        DB::table('users')->insert([
+            $user->save();
+        }
+
+        // Create admin
+        User::create([
             'name' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin'),
             'register_date' => now(),
             'score' => 0,
             'role' => 'admin',
-            'created_at' => now(),
-            'updated_at' => now(),
         ]);
     }
 }
