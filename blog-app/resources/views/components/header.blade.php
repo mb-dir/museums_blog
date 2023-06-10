e84f9c4a6d97de5b8eecb55582e4d541c2e88ebd
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-06-10 12:53:43 +0200

    Change header styles

--- blog-app/resources/views/components/header.blade.php
+++ blog-app/resources/views/components/header.blade.php
@@ -1,12 +1,13 @@
-<section class="relative h-96 bg-purple flex flex-col justify-center align-center text-center space-y-4 mb-4">
+<section class="relative h-screen flex flex-col justify-center align-center text-center space-y-4 mb-4 p-5"
+    style="background-image: url('{{asset('storage/bg.png')}}'); background-size: cover;">
     <div class="z-10">
-        <h1 class="text-6xl font-bold font-header uppercase text-white">
+        <h1 class="text-6xl font-bold font-header uppercase text-purple">
             Muzealne<span class="text-black"> Spojrzenie</span>
         </h1>
-        <p class="text-2xl text-gray-100 font-bold my-4">
+        <p class="text-2xl font-bold my-4">
             Czyli wędrówka po sztuce
         </p>
-        <p class="text-xs text-gray-100 my-4 pl-20 pr-20">
+        <p class="text-xs my-4 pl-20 pr-20">
             Zapraszamy do naszego forum o muzeach! Jeśli jesteś pasjonatem historii, kultury i sztuki, to miejsce to dla
             Ciebie.
             Przez nasze forum możesz poznać ciekawostki, fakty i opowieści związane z różnymi muzeami na całym świecie.
@@ -16,8 +17,8 @@
             do nas i
             pozwól nam zainspirować Twoją pasję!
         </p>
-        <p class="text-2xl text-gray-100">
-            Nie czekaj, <a class="text-black font-bold" href="/posts/create">podziel się</a> swoimi
+        <p class="text-2xl">
+            Nie czekaj, <a class="text-purple font-bold" href="/posts/create">podziel się</a> swoimi
             przemyśleniami!
         </p>
     </div>
